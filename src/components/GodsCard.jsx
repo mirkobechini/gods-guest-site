7848574753bef6cb7b59e3beed27cec48cac6415
mirkobechini <mirkobechini@gmail.com>
2026-02-15 12:11:17 +0100

    feat: implement GodIndexPage and GodsCard components with data fetching and display

---
+++ src/components/GodsCard.jsx
@@ -0,0 +1,45 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import GlobalContext from "../context/GlobalContext";
+
+export default function GodsCard({ god }) {
+
+    const { API_STORAGE_URL } = useContext(GlobalContext);
+
+    return (
+        <>
+            <div className="col">
+
+                <div className="card h-100" style={{ width: "100%", maxHeight: "100%" }}>
+                    <Link className="text-decoration-none text-dark" to={`/gods/${god.id}`}>
+
+                        <div className="card-img-top" style={{ aspectRatio: "1 / 1", overflow: "hidden" }}>
+                            <img
+                                className="img-fluid"
+                                src={`${API_STORAGE_URL}/${god.image}`}
+                                alt={god.name}
+                                style={{ width: "100%", height: "100%", objectFit: "cover", objectPosition: "center" }}
+                            />
+                        </div>
+
+                    </Link>
+                    <div className="card-body p-2">
+                        <div className="row">
+                            <span><strong>{god.name}</strong> - {god.title}</span>
+                            <div className="mt-2 d-flex gap-1">
+                                {
+                                    god.domains.map((domain)=>(
+                                        
+                                        <Link key={domain.id} to={`/domains/${domain.id}`}>
+                                            <i className={`${domain.icon} bg-secondary-subtle py-1`} style={{color:`${domain.color}`}}></i>
+                                            </Link>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/pages/gods/GodIndexPage.jsx
+++ src/pages/gods/GodIndexPage.jsx
@@ -1,7 +1,59 @@
+import { useContext, useEffect, useState } from "react";
+import GlobalContext from "../../context/GlobalContext";
+import axios from "axios";
+import GodsCard from "../../components/GodsCard";
+
 export default function GodIndexPage() {
+
+  const [gods, setGods] = useState([])
+  const [currentPantheon, setCurrentPantheon] = useState([])
+  const [errors, setError] = useState([])
+  const { API_URL_GODS } = useContext(GlobalContext)
+
+  function fetchData() {
+    axios.get(`${API_URL_GODS}`)
+      .then(res => {
+        setGods(res.data.data)
+        setCurrentPantheon(res.data.data[0].pantheon)
+      })
+      .catch(err => {
+        console.log(err.message);
+        setError({ error: err.message });
+      })
+  }
+
+  useEffect(fetchData, [])
+
+
+  //scorre l'array automaticamente
+
+  const groupedGods = gods.reduce((result, god) => { //reduce trasforma un array in un oggetto
+    const pantheonName = god.pantheon.name;
+    if (!result[pantheonName]) result[pantheonName] = [];
+    result[pantheonName].push(god);
+    return result;
+  }, {});
+
   return (
     <div>
       <h1>Gods</h1>
+      <div className="container my-4">
+        {
+          // groupedGods ->"chiave": pantheon, "valore": array di dei
+          Object.entries(groupedGods).map(([pantheon, gods]) => (
+            <div key={pantheon} className="mb-2 card p-3 border-0 shadow-sm rounded bg-primary-subtle">
+              <h2>{pantheon}</h2>
+              <div className="row row-cols-1 row-cols-md-4 row-cols-lg-6 g-3">
+                {
+                  gods.map((god) => (
+                    <GodsCard god={god} key={god.id} />
+                  ))
+                }
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 } 
