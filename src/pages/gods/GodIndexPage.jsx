ee2a75c9d5f68b43d48d4b10e7ec233b7085b2e9
mirkobechini <mirkobechini@gmail.com>
2026-02-15 12:30:08 +0100

    feat: update GodIndexPage to link pantheon names to their respective pages

--- src/pages/gods/GodIndexPage.jsx
+++ src/pages/gods/GodIndexPage.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import GlobalContext from "../../context/GlobalContext";
 import axios from "axios";
 import GodsCard from "../../components/GodsCard";
+import { Link } from "react-router-dom";
 
 export default function GodIndexPage() {
 
@@ -42,7 +43,7 @@ export default function GodIndexPage() {
           // groupedGods ->"chiave": pantheon, "valore": array di dei
           Object.entries(groupedGods).map(([pantheon, gods]) => (
             <div key={pantheon} className="mb-2 card p-3 border-0 shadow-sm rounded bg-primary-subtle">
-              <h2>{pantheon}</h2>
+              <Link className="h2 text-decoration-none text-dark" to={`/pantheons/${gods[0].pantheon?.id}`}>{pantheon}</Link>
               <div className="row row-cols-1 row-cols-md-4 row-cols-lg-6 g-3">
                 {
                   gods.map((god) => (
